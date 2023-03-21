c893dfc04dba529b96237b149128d0daf0af90f4
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-21 23:19:16 +0100

    oddioo

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,27 +2,27 @@ const s = document.getElementById('search-input')
 const container = document.getElementById('results-container')
 container.classList.add('d-none')
 
-s.addEventListener('keyup', () => {
-    console.log(s.value);
-    const search = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${s.value}`
-    fetch(search)
-        .then(r => r.json())
-        .then(artist => {
-            container.classList.remove('d-none')
-
-            const img = document.getElementById('artist')
-            const nomeArtista = document.getElementById('nome-artista')
-
-            for (let i = 0; i < artist.data.length; i++) {
-                img.src = artist.data[i].artist.picture
-                nomeArtista.textContent = artist.data[i].artist.name
+    s.addEventListener('keyup',() => {
+        const search = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${s.value}`
+        fetch(search)
+            .then(r => r.json())
+            .then(artist => {
+                container.classList.remove('d-none')
+    
+                const img = document.getElementById('artist')
+                const nomeArtista = document.getElementById('nome-artista')
+    
+                for (let i = 0; i < artist.data.length; i++) {
+                    img.src = artist.data[i].artist.picture
+                    nomeArtista.textContent = artist.data[i].artist.name
+                }
+    
             }
-        }
-        )
-        .then(()=>{
-            if(s.value === ''){
-                container.classList.add('d-none')
-            }
-        })
-}
-)
+            )
+            .then(()=>{
+                if(s.value === ''){
+                    container.classList.add('d-none')
+                }
+            })
+    }
+    )
