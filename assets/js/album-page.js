18da6864f6b10927e1aa77d0df7f8e9b9ae11f87
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-22 20:09:36 +0100

    commit

--- assets/js/album-page.js
+++ assets/js/album-page.js
@@ -2,23 +2,28 @@ const id = new URLSearchParams(window.location.search).get('id')
 const api = `https://striveschool-api.herokuapp.com/api/deezer/album/${id}`
 
 
-try{
-fetch(api)
-.then(r=>r.json())
-.then(albumPage => {
-    console.log(albumPage);
-    const imgCover = document.querySelector('.music-pic')
-    const album = document.getElementById('nome-album')
-    const albumNav = document.getElementById('nome-album-nav')
+try {
+    fetch(api)
+        .then(r => r.json())
+        .then(albumPage => {
+            console.log(albumPage);
+            const imgCover = document.querySelector('.music-pic')
+            const album = document.getElementById('nome-album')
+            const albumNav = document.getElementById('nome-album-nav')
+            const name = document.getElementById('nome-artista')
+            const icon = document.getElementById('icon')
 
-    const a= [album, albumNav]
-    for(albums of a){
-        albums.innerText = albumPage.title
-    }
+            const a = [album, albumNav]
+            for (albums of a) {
+                albums.innerText = albumPage.title
+            }
+            name.innerHTML = `${albumPage.artist.name} &bull; <span class="fw-light"> ${albumPage.release_date.slice(0, 4)}</span> &bull;
+            <span class="fw-light"> ${albumPage.nb_tracks} brani, </span><span class="fw-light opacity-75">${albumPage.duration.toString().slice(0,2)} min ${albumPage.duration.toString().slice(2,4)} sec.</span>`
 
-    imgCover.src = albumPage.cover
-})
+            imgCover.src = albumPage.cover
+            icon.src = albumPage.artist.picture_small
+        })
 }
-catch(err){
+catch (err) {
     document.querySelector("body").innerHTML = err.message;
 }
