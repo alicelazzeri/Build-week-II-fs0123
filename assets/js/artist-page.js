4b23ef6ba1c4548ee13746a166766bfa3cef0e47
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-22 23:02:54 +0100

    commit

--- assets/js/artist-page.js
+++ assets/js/artist-page.js
@@ -17,3 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
     //nel css c'è il tag nav con {transition: 2s} per non rendere il cambio colore netto
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let scroll = window.scrollY;
+
+  const button = document.getElementById("btn-app");
+
+  //diminuire il numero se si vuole che il cambio colore arrivi prima
+  const scrollChange = 430;
+
+  window.addEventListener("scroll", function () {
+    scroll = window.scrollY;
+
+    // bg-black è la classe di bootstrap, basta cambiarlo con qualsiasi classe che si voglia
+    scroll >= scrollChange ? button.classList.remove('d-none') : button.classList.add('d-none')
+
+    //nel css c'è il tag nav con {transition: 2s} per non rendere il cambio colore netto
+  });
+});
